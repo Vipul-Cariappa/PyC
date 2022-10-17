5bdcece69845da984378ef4ad7e8602bd7f31482
Vipul Cariappa <vipulcariappa@gmail.com>
2022-10-17 15:04:29 +0530

    updated

--- .gitpod.run.sh
+++ .gitpod.run.sh
@@ -1,12 +1,12 @@
 #/usr/bin/sh
 
 printf "\nBuilding PyC\n"
-if ! make ; then
+if ! make config=memorytest ; then
     exit 1
 fi
 
 printf "\nCopying Files\n" 
-if ! cp ./bin/Debug/libPyC.so ./tests/PyC.so ; then
+if ! cp ./bin/MemoryTest/libPyC.so ./tests/PyC.so ; then
     exit 1
 fi
 
@@ -19,22 +19,6 @@ if ! gcc -g -Wall -shared -fPIC -I./tests/c/ -o tests/libcmodule.so tests/c/modu
 fi
 
 printf "\nRunning the tests\n"
-if ! python3 tests/test.py ; then
+if ! python3.10d -X dev -X showrefcount tests/test.py -v ; then
     exit 1
 fi
-
-printf "\nBuilding PyC to Check Memory Leaks\n"
-if ! make config=memorytest ; then
-    exit 1
-fi
-
-printf "\nCopying Files\n" 
-if ! cp ./bin/MemoryTest/libPyC.so ./tests/PyC.so ; then
-    exit 1
-fi
-
-printf "\nRunning tests to Check Memory Leaks\n"
-if ! python3.10d tests/test.py ; then
-    exit 1
-fi
-
